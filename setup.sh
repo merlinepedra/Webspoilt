639bc4deddcd0d9bf9540eaa27ec7c7f4e7f1bdc
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2021-07-29 14:43:02 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-echo  collecting the required modules
-echo  started Downloading
-sudo apt install python3-pip
-echo pip Installed 
-sudo apt insatll tdqm
-echo Sucessfully Download  
-sudo apt install pyfiglet 
-echo Important libaries downloaded Sucessfuly 
